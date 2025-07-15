3888ebdd48eec65251d3b4c2c6e8480f5f177f8e
RashTechInfo <rashtechinfo@gmail.com>
2025-07-15 14:09:30 +0530

    update file

--- src/routers/admin/admin_profile_update.js
+++ src/routers/admin/admin_profile_update.js
@@ -5,18 +5,11 @@ const multer = require("multer");
 const {
   Update_admin,
 } = require("../../controllers/admin/admin_profile_update");
-const path = require("path");
-const fs = require("fs");
-const uploadPath = path.join(__dirname, "../public/uploads");
 
-// Ensure the folder exists
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
 let upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, uploadPath);
+      cb(null, "public/uploads");
     },
     filename: (req, file, cb) => {
       cb(null, Date.now() + "_" + file.originalname);
