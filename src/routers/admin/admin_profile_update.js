521699c2523694e3c32cb4eb534ef6044bca126a
RashTechInfo <rashtechinfo@gmail.com>
2025-07-15 11:57:53 +0530

    update file

--- src/routers/admin/admin_profile_update.js
+++ src/routers/admin/admin_profile_update.js
@@ -9,7 +9,7 @@ const {
 let upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "public/uploads");
+      cb(null, "../../../public/uploads");
     },
     filename: (req, file, cb) => {
       cb(null, Date.now() + "_" + file.originalname);
